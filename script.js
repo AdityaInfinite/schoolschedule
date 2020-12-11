abbb81e50917076dcbc466485a602f85d4caac27
aditya <dalvi.aditya14@gmail.com>
2020-12-11 22:42:32 +0530

    Some Changes

--- script.js
+++ script.js
@@ -1,65 +1,98 @@
-var date = new Date();
-var time = date.getTime();
-var day = date.getDay();
+var date;
+var time;
+var day;
 // format of cell id is (period)(day)
+setInterval(doAll, 1000);
+function doAll() {
+    showTime();
+
+    periodHighlight();
+}
+function showTime() {
+    date = new Date("December 10, 2020 8:31:00");
+    hour = date.getHours();
+    min = date.getMinutes();
+    sec = date.getSeconds();
+    day = date.getDay();
+    am_pm = "AM";
+
+    if (hour > 12) {
+        hour -= 12;
+        am_pm = "PM";
+    }
+    if (hour == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    let currentTime = hour + ":"
+        + min + ":" + sec + am_pm;
+
+    document.getElementById("clock")
+        .innerHTML = currentTime;
+}
+doAll();
 dayHighlight();
-periodHighlight();
 function dayHighlight() {
     var x = document.getElementById("myTable").rows[0].cells;
     var y = document.getElementById("myTable").rows;
-
-    console.log(checkPeriod(12,0,1,40));
-    $(`#${8}${day}`).html("hi");
-
-    $(`#${day}`).toggleClass("success");
-    console.log(x[day]);
+    // console.log(checkPeriod(12, 0, 1, 40));
+    console.log(day)
+    $(`#day${day}`).toggleClass("success");
+    console.log(x[day].innerText);
     // $(x[day]).css("background-color", "red");
     // $(x[day]).css("border-color", "blue");
-    document.getElementById("btn").innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    // document.getElementById("btn").innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
     //$(`#${1}${day}`).html("hi");
 }
-function periodHighlight(){
-    if(checkPeriod(8,30,9,10)){
-        console.log(document.getElementById(`${1}${day}`).innerHTML);       
-        $(`#${1}${day}`).html("hi");
+function periodHighlight() {
+    var ClasTim = [
+        [8, 30, 9, 10],
+        [9, 20, 10, 0],
+        [9, 59, 10, 19],
+        [10, 20, 11, 00],
+        [11, 10, 11, 50],
+        [11, 51, 12, 9],
+        [12, 10, 12, 50],
+        [1, 0, 1, 40]
+    ];
+    for (let period = 0; period < ClasTim.length; period++) {
+        // if (period === 2 || period === 5) {
+        //     console.log(document.getElementById(`${period}0`).innerText);
+        // }else{
+        //     console.log(document.getElementById(`${period}${day}`).innerText);
+        // }
+
+        if (checkPeriod(ClasTim[period][0], ClasTim[period][1], ClasTim[period][2], ClasTim[period][3])) {
+            // $(`#${period}${day}`).html("hi");
+            // console.log(period);
+            if (period === 2 || period === 5) {
+                $(`#${period}0`).html("hi");
+                document.getElementById("NowPeriod").innerText = document.getElementById(`${period}0`).innerHTML
+            }else{
+                document.getElementById("NowPeriod").innerText = document.getElementById(`${period}${day}`).innerText;
+                console.log(document.getElementById(`${period}${day}`).innerText);
+            }
+
+        }else{
+            console.log("no class");
+        }
     }
-    else if(checkPeriod(9,20,10,0)){
-        console.log(document.getElementById(`${2}${day}`).innerHTML);
-        $(`#${2}${day}`).html("hi");
-    }
-    else if(checkPeriod(9,59,10,19)){
-        console.log("break")
-        //$(`#${}${day}`).html("hi");
-    }
-    else if(checkPeriod(10,20,11,00)){
-        console.log(document.getElementById(`${3}${day}`).innerHTML);
-        $(`#${3}${day}`).html("hi");
-    }
-    else if(checkPeriod(11,10,11,50)){
-        console.log(document.getElementById(`${4}${day}`).innerHTML);
-        $(`#${4}${day}`).html("hi");
-    }
-    else if(checkPeriod(11,51,12,9)){
-        console.log("break")
-        //$(`#${}${day}`).html("hi");
-    }
-    else if(checkPeriod(12,10,12,50)){
-        console.log(document.getElementById(`${5}${day}`).innerHTML);
-        $(`#${5}${day}`).html("hi");
-    }
-    else if(checkPeriod(1,0,1,40)){
-        console.log(document.getElementById(`${6}${day}`).innerHTML);
-        $("#NowPeriod").html(document.getElementById(`${6}${day}`).innerHTML);
-        $(`#${6}${day}`).html("hi");
-    }
+
+
 }
 
-function checkPeriod(StartHour,StartMinute,EndHour,EndMinute){
-    if( (date.getHours() >= StartHour) &&  (date.getMinutes() >= StartMinute) && (date.getHours() <= EndHour) &&  (date.getMinutes() <= EndMinute)){
+function checkPeriod(StartHour, StartMinute, EndHour, EndMinute) {
+    if ((date.getHours() >= StartHour) && (date.getMinutes() >= StartMinute) && (date.getHours() <= EndHour) && (date.getMinutes() <= EndMinute)) {
         return true;
     }
-    else{
-        console.log(`st=${StartHour}:${StartMinute} now=${date.getHours()}:${date.getMinutes()}`)
+    else {
+        // console.log(`st=${StartHour}:${StartMinute} now=${date.getHours()}:${date.getMinutes()}`)
         return false;
     }
 }
+
