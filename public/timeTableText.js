5c0878ec86a7b4fbe684867f7476f1a08a40ffa6
aditya <dalvi.aditya14@gmail.com>
2020-12-18 21:34:15 +0530

    added index

--- public/timeTableText.js
+++ public/timeTableText.js
@@ -12,13 +12,12 @@ async function classSelected(){
         },
         body: JSON.stringify(data)
     }
-    const responce = await fetch("/api", options);
+    const responce = await fetch("/getdata", options);
     json = await responce.json();
     console.log(json.timeTable)
 
     for (let day = 1; day <= 5 ; day++) {
         for (let period = 0; period <= 7; period++) {
-            console.log(json.timeTable[period][day-1]);
             $(`#${period}${day}`).html(json.timeTable[period][day-1]);
         }   
     }
