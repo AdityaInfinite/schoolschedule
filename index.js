27a514388e859b1f4cfa9519a735fa9d2cc18a4b
Aditya <dalvi.aditya14@gmail.com>
2021-07-28 23:09:52 +0530

    9c

--- index.js
+++ index.js
@@ -69,7 +69,3 @@ app.get('/logs', (request, response) => {
         response.json(data);
     });
 });
-app.get('/new', (request, response) => {
-    response.sendFile(`${__dirname.replace(/\\/g, "/")}/pub/newTable/index.html`)
-});
-
